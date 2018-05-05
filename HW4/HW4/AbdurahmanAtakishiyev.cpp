c5279e418da4af82ad4dbd470b428a5e1784c0b0
Abdurahman <kuyoku4ever@gmail.com>
2018-05-05 11:43:11 +0300

    some extra stuff

--- HW4/HW4/AbdurahmanAtakishiyev.cpp
+++ HW4/HW4/AbdurahmanAtakishiyev.cpp
@@ -47,14 +47,14 @@ typedef struct {
 	float radius;
 }planet_t;
 
-#define NUM 4
+int size = 4;
 
-light_t light[4] =
+light_t light[24] =
 {
+	{ { 0, 0 },{ 1, 1, 0 },{ 0, 0 } },
 	{ { 0, 0 },{ 1, 0, 0 },{ 3,  2 } },
 	{ { 200, 0 },{ 0, 1, 0 },{ -2, -1 } },
-	{ { -200, 0 },{ 0, 0, 1 },{ 3, -2 } },
-	{ { 0, 0 },{ 1, 1, 1 },{ 0, 0 } }
+	{ { -200, 0 },{ 0, 0, 1 },{ 3, -2 } }
 };
 
 planet_t planet[3];
@@ -194,7 +194,7 @@ void drawPlanet(planet_t p, float radius) {
 		P.N = unitV(subV({ x, y }, { radius*cos(ang*D2R), radius*sin(ang*D2R) }));
 
 		color_t res = { 0, 0, 0 };
-		for (int i = 0; i < NUM; i++) {
+		for (int i = 0; i < size; i++) {
 			res = addColor(res, calculateColor(light[i], P));
 		}
 		glColor3f(res.r, res.g, res.b);
@@ -211,14 +211,14 @@ void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	// r,g,b sources 
-	for (int i = 0; i < NUM - 1; i++) {
+	for (int i = 1; i < size; i++) {
 		glColor3f(light[i].color.r, light[i].color.g, light[i].color.b);
 		circle(light[i].pos.x, light[i].pos.y, 10);
 	}
 
 	// sun
-	glColor3f(light[3].color.r, light[3].color.g, light[3].color.b);
-	circle(light[3].pos.x, light[3].pos.y, 40);
+	glColor3f(light[0].color.r, light[0].color.g, light[0].color.b);
+	circle(light[0].pos.x, light[0].pos.y, 50);
 	
 	// planets
 	float radius = 200;
@@ -228,21 +228,30 @@ void display() {
 	}
 
 	// keyboard hints
-	glColor3f(1, 1, 1);
+	glColor3f(1, 0, 0);
 	vprint(-380, 380, GLUT_BITMAP_9_BY_15, "<F1>Red");
-	vprint(-375, 365, GLUT_BITMAP_9_BY_15, "%s", state(light[0].color.r));
-
+	vprint(-375, 365, GLUT_BITMAP_9_BY_15, "%s", state(light[1].color.r));
+	
+	glColor3f(0, 1, 0);
 	vprint(-300, 380, GLUT_BITMAP_9_BY_15, "<F2>Green");
-	vprint(-295, 365, GLUT_BITMAP_9_BY_15, "%s", state(light[1].color.r));
+	vprint(-295, 365, GLUT_BITMAP_9_BY_15, "%s", state(light[2].color.r));
 
+	glColor3f(0, 0, 1);
 	vprint(-200, 380, GLUT_BITMAP_9_BY_15, "<F3>Blue");
-	vprint(-195, 365, GLUT_BITMAP_9_BY_15, "%s", state(light[2].color.r));
+	vprint(-195, 365, GLUT_BITMAP_9_BY_15, "%s", state(light[3].color.r));
 
+	glColor3f(1, 1, 0);
 	vprint(-110, 380, GLUT_BITMAP_9_BY_15, "<F4>Sun");
-	vprint(-105, 365, GLUT_BITMAP_9_BY_15, "%s", state(light[3].color.r));
+	vprint(-105, 365, GLUT_BITMAP_9_BY_15, "%s", state(light[0].color.r));
 
-	vprint(-20, 380, GLUT_BITMAP_9_BY_15, "<F5>Pause");
-	vprint(80, 380, GLUT_BITMAP_9_BY_15, "<F6>Restart");
+	glColor3f(1, 1, 1);
+	vprint(-380, 340, GLUT_BITMAP_9_BY_15, "<F5>Pause");
+	vprint(-380, 320, GLUT_BITMAP_9_BY_15, "<F6>Restart");
+	vprint(-380, 300, GLUT_BITMAP_9_BY_15, "<F7>Add random light");
+
+	vprint(300, 380, GLUT_BITMAP_9_BY_15, "Abdurahman");
+	vprint(295, 360, GLUT_BITMAP_9_BY_15, "Atakishiyev");
+	vprint(320, 340, GLUT_BITMAP_9_BY_15, "HW #4");
 
 	glutSwapBuffers();
 
@@ -284,29 +293,34 @@ void onSpecialKeyDown(int key, int x, int y)
 	case GLUT_KEY_LEFT: left = true; break;
 	case GLUT_KEY_RIGHT: right = true; break;
 	case GLUT_KEY_F1:
-		if (light[0].color.r == 0.18f)
-			light[0].color = { 1,0,0 };
-		else
-			light[0].color = { 0.18f,0.3f,0.3f }; break;
-	case GLUT_KEY_F2:
 		if (light[1].color.r == 0.18f)
-			light[1].color = { 0,1,0 };
+			light[1].color = { 1,0,0 };
 		else
 			light[1].color = { 0.18f,0.3f,0.3f }; break;
-	case GLUT_KEY_F3:
+	case GLUT_KEY_F2:
 		if (light[2].color.r == 0.18f)
-			light[2].color = { 0,0,1 };
+			light[2].color = { 0,1,0 };
 		else
 			light[2].color = { 0.18f,0.3f,0.3f }; break;
-	case GLUT_KEY_F4:
+	case GLUT_KEY_F3:
 		if (light[3].color.r == 0.18f)
-			light[3].color = { 1,1,1 };
+			light[3].color = { 0,0,1 };
 		else
 			light[3].color = { 0.18f,0.3f,0.3f }; break;
+	case GLUT_KEY_F4:
+		if (light[0].color.r == 0.18f)
+			light[0].color = { 1,1,0 };
+		else
+			light[0].color = { 0.18f,0.3f,0.3f }; break;
 	case GLUT_KEY_F5:
 		timer = !timer; break;
 	case GLUT_KEY_F6:
 		Init();
+		break;
+	case GLUT_KEY_F7:
+		light[size] = { { 0, 0 },{ (float)rand() / RAND_MAX, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX },{ 3, -2 } };
+		size++;
+		break;
 	}
 	// to refresh the window it calls display() function
 	glutPostRedisplay();
@@ -391,7 +405,7 @@ void onTimer(int v) {
 			if (planet[i].angle > 360)
 				planet[i].angle -= 360;
 		}
-		for (int i = 0; i < NUM; i++) {
+		for (int i = 0; i < size; i++) {
 			light[i].pos = addV(light[i].pos, light[i].vel);
 
 			// Reflection from Walls.
@@ -421,6 +435,8 @@ void Init() {
 		planet[i].angle = rand() % 360;
 		planet[i].speed = rand() % 2 + rand()%2 + 1;
 		planet[i].radius = rand() % 10 + 20;
+	}
+	for (int i = 1; i < size; i++) {
 		light[i].pos = { rand() % 801 - 400.0f, rand() % 801 - 400.0f };
 	}
 }
@@ -430,7 +446,7 @@ void main(int argc, char *argv[]) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-	glutCreateWindow("Light Simulation by Abdurahman Atakishiyev");
+	glutCreateWindow("HW4 - Light Simulation by Abdurahman Atakishiyev");
 
 	glutDisplayFunc(display);
 	glutReshapeFunc(onResize);
